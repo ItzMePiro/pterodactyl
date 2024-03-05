26dff36d70bd251ba9cca14ef59f73e4d4b34154
ItzMePiro <142495487+ItzMePiro@users.noreply.github.com>
2024-03-05 21:12:44 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -8,20 +8,22 @@ import string
 TOKEN = 'YOUR_DISCORD_BOT_TOKEN'
 VPS_IP = 'YOUR_VPS_IP'
 SSH_USERNAME = 'YOUR_SSH_USERNAME'
-PRIVATE_KEY_PATH = 'PATH_TO_YOUR_PRIVATE_KEY'
+PRIVATE_KEY_PATH = '/root/.ssh/id_rsa'
 
 # Bot setup
 bot = commands.Bot(command_prefix='!')
 
-# Function to create a container
+# Function to create a container with custom SSH port
 def create_container():
     container_id = ''.join(random.choices(string.ascii_letters + string.digits, k=8))
+    ssh_port = random.randint(49152, 65535)  # Randomly select a port in the dynamic/private port range
+    ssh_command = f'docker run -d -p {ssh_port}:22 --name {container_id} ubuntu'
     ssh_client = paramiko.SSHClient()
     ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
     ssh_client.connect(hostname=VPS_IP, username=SSH_USERNAME, key_filename=PRIVATE_KEY_PATH)
-    stdin, stdout, stderr = ssh_client.exec_command(f'docker run -d --name {container_id} ubuntu')
+    stdin, stdout, stderr = ssh_client.exec_command(ssh_command)
     ssh_client.close()
-    return container_id
+    return container_id, ssh_port
 
 # Bot events
 @bot.event
@@ -31,8 +33,8 @@ async def on_ready():
 # Bot commands
 @bot.command(name="create-container")
 async def create_container_command(ctx):
-    container_id = create_container()
-    await ctx.send(f"Container created with ID: {container_id}")
+    container_id, ssh_port = create_container()
+    await ctx.send(f"Container created with ID: {container_id}, SSH port: {ssh_port}")
 
 # Run the bot
 bot.run(TOKEN)
