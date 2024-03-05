c2d6c58f8d38342502277d648ff174ef39036020
ItzMePiro <142495487+ItzMePiro@users.noreply.github.com>
2024-03-05 21:09:34 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,46 +1,38 @@
 import discord
 from discord.ext import commands
-import subprocess
+import paramiko
 import random
 import string
 
-intents = discord.Intents.default()
-intents.typing = True
-intents.presences = True
+# Configuration
+TOKEN = 'YOUR_DISCORD_BOT_TOKEN'
+VPS_IP = 'YOUR_VPS_IP'
+SSH_USERNAME = 'YOUR_SSH_USERNAME'
+PRIVATE_KEY_PATH = 'PATH_TO_YOUR_PRIVATE_KEY'
 
-bot = commands.Bot(command_prefix='!', intents=intents)
+# Bot setup
+bot = commands.Bot(command_prefix='!')
 
-authorized_users = ['itzmepiro']  # Replace with authorized Discord usernames
-container_info = {}  # Dictionary to store container information
+# Function to create a container
+def create_container():
+    container_id = ''.join(random.choices(string.ascii_letters + string.digits, k=8))
+    ssh_client = paramiko.SSHClient()
+    ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    ssh_client.connect(hostname=VPS_IP, username=SSH_USERNAME, key_filename=PRIVATE_KEY_PATH)
+    stdin, stdout, stderr = ssh_client.exec_command(f'docker run -d --name {container_id} ubuntu')
+    ssh_client.close()
+    return container_id
 
+# Bot events
 @bot.event
 async def on_ready():
     print(f'Logged in as {bot.user}')
 
-@bot.command(name="vm-create")
-async def create_container(ctx, name: str):
-    if str(ctx.author) not in authorized_users:
-        await ctx.send("You are not authorized to use this command.")
-        return
-    
-    container_id = ''.join(random.choices(string.ascii_letters + string.digits, k=8))
-    ssh_password = ''.join(random.choices(string.ascii_letters + string.digits, k=12))
-    ssh_port = random.randint(10000, 60000)  # Generate a random port between 10000 and 60000
-    
-    # Run Docker command to create container
-    subprocess.run(f"docker run -d -p {ssh_port}:22 --memory=1g --cpus=0.5 --name {container_id} ubuntu", shell=True)
-    
-    # Set SSH password
-    subprocess.run(f"docker exec {container_id} bash -c 'echo \"root:{ssh_password}\" | chpasswd'", shell=True)
-    
-    # Store container information
-    container_info[container_id] = {'ssh_password': ssh_password, 'ssh_port': ssh_port}
-    
-    # Send container information to user's DM
-    user = ctx.author
-    dm_channel = await user.create_dm()
-    await dm_channel.send(f"Container {name} created. SSH port: {ssh_port}, SSH password: {ssh_password}")
-    await dm_channel.send("To SSH into the VPS, use the following command:\n"
-                          f"`ssh root@YOUR_SERVER_IP -p {ssh_port}`")
+# Bot commands
+@bot.command(name="create-container")
+async def create_container_command(ctx):
+    container_id = create_container()
+    await ctx.send(f"Container created with ID: {container_id}")
 
-bot.run('')
+# Run the bot
+bot.run(TOKEN)
